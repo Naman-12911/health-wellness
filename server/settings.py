6adb57f7aa71cfe3237a83595c90e08564c02b4a
Naman-12911 <namangarg820@gmail.com>
2024-09-07 20:02:20 +0530

    add helath

--- server/settings.py
+++ server/settings.py
@@ -208,7 +208,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     "http://127.0.0.1:8000",
-    "https://health-wellness-0bdd7416387d.herokuapp.com"
+    "https://health-wellness-0bdd7416387d.herokuapp.com/"
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
